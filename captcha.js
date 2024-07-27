612a24e14ce0729347a1027f9e9d7d7579c3f968
Karthik S <karthiani000@gmail.com>
2024-07-27 13:00:02 +0530

    Add files via upload

---
+++ captcha.js
@@ -0,0 +1,40 @@
+let captcha;
+function generate() {
+
+
+    document.getElementById("submit").value = "";
+
+
+    captcha = document.getElementById("image");
+    let uniquechar = "";
+
+    const randomchar =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    // Generate captcha for length of
+    // 5 with random character
+    for (let i = 1; i < 5; i++) {
+        uniquechar += randomchar.charAt(
+            Math.random() * randomchar.length)
+    }
+
+    // Store generated input
+    captcha.innerHTML = uniquechar;
+}
+
+function printmsg() {
+    const usr_input = document
+        .getElementById("submit").value;
+
+    
+    if (usr_input == captcha.innerHTML) {
+        let s = document.getElementById("key")
+            .innerHTML = "Matched";
+        generate();
+    }
+    else {
+        let s = document.getElementById("key")
+            .innerHTML = "not Matched";
+        generate();
+    }
+}
